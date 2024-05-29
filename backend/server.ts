4f1ba732ac47b583d829295856a4ec456bbbe0c7
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-05-29 09:07:54 +0530

    Updating the server file
    Signed-off-by: narasimha-1511 <s.narasimha.2005@gmail.com>

--- backend/server.ts
+++ backend/server.ts
@@ -1,50 +1,36 @@
-import Express, {Response , Request , Express as TExpress } from 'express';
-const cors = require('cors');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
+import { Response, Request } from "express";
+const Express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-import authRoutes from './routes/auth.routes';
-import messageRoutes from './routes/messages.rotues';
-import userRoutes from './routes/user.routes';
+import authRoutes from "./routes/auth.routes";
+import messageRoutes from "./routes/messages.rotues";
+import { userRoutes } from "./routes/user.routes";
 
-import connectToMongo from './db/connectToMongo';
-import { IDatabase } from './interfaces';
+import connectToMongo from "./db/connectToMongo";
+import { IDatabase } from "./interfaces";
 
 dotenv.config();
-const app = Express();  
+const app = Express();
+const __dirnamee = path.resolve();
 
 app.use(cors());
 app.use(Express.json());
 app.use(cookieParser());
 
-app.get('/', (req:Request , res:Response ) => {
-    res.send('Hello World!');
-});
+app.use("/auth", authRoutes);
+app.use("/messages", messageRoutes);
+app.use("/users", userRoutes);
 
-app.use('/auth', authRoutes);
-app.use('/messages', messageRoutes)
-app.use('/users', userRoutes);
+app.use(Express.static(path.join(__dirnamee, "/chat-app/dist")));
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirnamee, "chat-app", "dist", "index.html"));
+});
 
 app.listen(process.env.PORT, async () => {
-    await connectToMongo();
-    console.log(`Server is running on port ${process.env.PORT}`);
+  await connectToMongo();
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-export default class Server{
-
-    db: IDatabase;
-    engine: TExpress;
-
-    constructor(database : IDatabase){
-        this.engine = Express();
-        this.db = database;
-    }
-
-    #defaultServerStart(){
-
-    }
-
-    start() {
-        this.#defaultServerStart();
-    }
-}
