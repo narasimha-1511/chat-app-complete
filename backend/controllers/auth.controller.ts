be215b1b5d4d0e147d64d79778d94d4693b67fd0
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-05-29 09:08:16 +0530

    Updating controllers
    Signed-off-by: narasimha-1511 <s.narasimha.2005@gmail.com>

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,129 +1,119 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
-import bcrypt from "bcryptjs";
+const bcrypt = require("bcryptjs");
 import generateTokenAndSetCookie from "../utils/generateJWT";
 
 export default class AuthController {
+  static async login(req: Request, res: Response) {
+    try {
+      const { userName, password } = req.body;
 
-    static async login(req: Request, res: Response) {
-        
-        try{
-            const {userName, password} = req.body;
+      const user = await User.findOne({ userName });
 
-            const user = User.findOne({userName});
+      if (!user) {
+        return res.status(400).json({
+          error: "User not found",
+        });
+      }
 
-            if(!user){
-                return res.status(400).json({
-                    "message":"User not found"
-                })
-            }
-            
-            //@ts-ignore
-            const isMatch = await bcrypt.compare(password, user.password);
+      //@ts-ignore
+      const isMatch = await bcrypt.compare(password, user.password);
 
-            if(!isMatch){
-                return res.status(400).json({
-                    "message":"Invalid Credentials"
-                })
-            }
+      if (!isMatch) {
+        return res.status(400).json({
+          error: "Invalid Credentials",
+        });
+      }
 
-            //@ts-ignore
-            generateTokenAndSetCookie(user.id , res);
+      //@ts-ignore
+      generateTokenAndSetCookie(user._id, res);
 
-            res.status(200).json({
-                //@ts-ignore
-                _id: user.id,
-                //@ts-ignore
-                fullName: user.fullName,
-                //@ts-ignore
-                userName: user.userName,
-                //@ts-ignore
-                profilePic: user.profilePic,
-            })
-
-        }
-        catch(error){
-            console.log("Error in login Controller", error)
-            res.status(500).json({
-                message:" Internal Server Error"
-            });
-        }
+      res.status(200).json({
+        //@ts-ignore
+        _id: user._id,
+        //@ts-ignore
+        fullName: user.fullName,
+        //@ts-ignore
+        userName: user.userName,
+        //@ts-ignore
+        profilePic: user.profilePic,
+      });
+    } catch (error) {
+      console.log("Error in login Controller", error);
+      res.status(500).json({
+        error: " Internal Server Error",
+      });
     }
+  }
 
-    static async signup(req: Request, res: Response) {
-        
-        try{
-            const {fullName, userName, password , confirmPassword, gender} = req.body;
-            
-            if(password != confirmPassword){
-                return res.status(400).json({
-                    "error":"passswords don't match"
-                })
-            }
+  static async signup(req: Request, res: Response) {
+    try {
+      const { fullName, userName, password, confirmPassword, gender } =
+        req.body;
 
-            const user =  await User.findOne({userName});
+      if (password != confirmPassword) {
+        return res.status(400).json({
+          error: "passswords don't match",
+        });
+      }
 
-            if(user){
-                return res.status(400).json({
-                    "error":"User already exists"
-                })
-            }
+      const user = await User.findOne({ userName });
 
-            //take the password and hash it     
-            const hashedPassword = await bcrypt.hash(password, 10);
+      if (user) {
+        return res.status(400).json({
+          error: "User already exists",
+        });
+      }
 
-            const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
-            const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+      //take the password and hash it
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-            const newUser = new User({
-                fullName: fullName,
-                userName: userName,
-                password: hashedPassword,
-                gender: gender,
-                profilePic: (gender == "male") ? boyProfilePic : girlProfilePic, 
-            });
+      const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+      const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
 
-            await newUser.save();
+      const newUser = new User({
+        fullName: fullName,
+        userName: userName,
+        password: hashedPassword,
+        gender: gender,
+        profilePic: gender == "male" ? boyProfilePic : girlProfilePic,
+      });
 
-            if(newUser){
+      await newUser.save();
 
-                generateTokenAndSetCookie(newUser.id , res);
-    
-                res.status(201).json({
-                    _id: newUser.id,
-                    fullName: newUser.fullName,
-                    userName: newUser.userName,
-                    profilePic: newUser.profilePic,
-                })
-            }
-            else{
-                res.status(400).json({
-                    "error":"User not created error with data given"
-                })
-            }
+      if (newUser) {
+        generateTokenAndSetCookie(newUser.id, res);
 
-        }
-
-        catch(error){
-            console.log("Error in signup Controller", error)
-            res.status(500).json({
-                message:" Internal Server Error"
-            });
-        }
+        res.status(201).json({
+          _id: newUser.id,
+          fullName: newUser.fullName,
+          userName: newUser.userName,
+          profilePic: newUser.profilePic,
+        });
+      } else {
+        res.status(400).json({
+          error: "User not created error with data given",
+        });
+      }
+    } catch (error) {
+      console.log("Error in signup Controller", error);
+      res.status(500).json({
+        error: " Internal Server Error",
+      });
     }
+  }
 
-    static async logout(req: Request, res: Response) {
-        try{
-            res.clearCookie("token");
-            res.status(200).json({
-                message:"Logged Out Successfully"
-            })
-        }
-        catch(error){
-            console.log("Error in logout Controller", error)
-            res.status(500).json({
-                message:" Internal Server Error"
-            });
-        }
+  static async logout(req: Request, res: Response) {
+    try {
+      // res.clearCookie("token");
+      res.status(200).json({
+        message: "Logged Out Successfully",
+      });
+    } catch (error) {
+      console.log("Error in logout Controller", error);
+      res.status(500).json({
+        error: " Internal Server Error",
+      });
     }
+  }
 }
--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -6,20 +6,26 @@ export default class UserController {
 
     static async getUsersforSidebar(req: CustomRequset, res:Response ) {
         try {
+          // @ts-ignore
+          const loggedInUserId = req.user._id;
 
-            const loggedInUserId = req.user._id;
-            
-            const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
-            
-            res.status(200).json({
-                users: filteredUsers
-            });
-        
+          console.log("Logged in User ID", loggedInUserId);
+          console.log("data requested")
+
+          const filteredUsers = await User.find({
+            _id: { $ne: loggedInUserId },
+          }).select("-password");
+
+          console.log("Filtered Users", filteredUsers);
+
+          res.status(200).json({
+            users: filteredUsers,
+          });
         }
         catch (error) {
             console.error("Error in get Users for Sidebar Controller", error);
             res.status(500).json({
-                message: "Internal Server Error"
+                error: "Internal Server Error"
             });
         }
     }
