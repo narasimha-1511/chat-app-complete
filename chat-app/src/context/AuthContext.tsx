986135220219941424b200ff61a6e936b32d3142
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-05-29 09:09:59 +0530

    Adding the context
    Signed-off-by: narasimha-1511 <s.narasimha.2005@gmail.com>

--- chat-app/src/context/AuthContext.tsx
+++ chat-app/src/context/AuthContext.tsx
@@ -1,19 +1,52 @@
-import { createContext, useContext, useState } from "react";
-import React from "react";
+import React, {
+  Dispatch,
+  FC,
+  SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
-export const AuthContext = createContext(0);
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const useAuthContext = () => {
-  return useContext(AuthContext);
+export type AuthUser = {
+  fullName: string;
+  userName: string;
+  _id: string;
+  profilePic: string;
 };
 
-export const AuthContextProvider = ({ children }) => {
-  const [authUser, setAuthUser] = useState(
-    JSON.parse(localStorage.getItem("chat-user") || "")
-  );
+export const AuthContext = createContext<Partial<AuthContextType>>({});
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export interface AuthContextType {
+  authUser: AuthUser | undefined;
+  setAuthUser: Dispatch<SetStateAction<AuthUser>> | undefined;
+}
+
+export default function AuthContextProvider({ children }) {
+  const [authUser, setAuthUser] = useState<AuthUser>({
+    fullName: "",
+    userName: "",
+    _id: "",
+    profilePic: "",
+  });
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("chat-user");
+    try {
+      if (storedUser) {
+        const user = JSON.parse(storedUser);
+        setAuthUser(user);
+      }
+    } catch (error) {
+      console.error("Error while parsing the json data", error);
+    }
+  }, []);
 
   return (
-    <AuthContext.Provider value={authUser}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ authUser, setAuthUser }}>
+      {children}
+    </AuthContext.Provider>
   );
-};
+}
