af27947a43a6f40db9aea84a3d796536fdcd47e8
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-05-30 11:01:37 +0530

    added the socket functionality

--- backend/controllers/messages.controller.ts
+++ backend/controllers/messages.controller.ts
@@ -1,89 +1,88 @@
 import Message from "../models/message.model";
 import { Request, Response } from "express";
 import Conversation from "../models/conversation.model";
+import { getReceiverSocketId, io } from "../socket/socket";
 
-export default class MessageController{
+export default class MessageController {
+  static async sendMessage(req: Request, res: Response) {
+    try {
+      const { id: receiverID } = req.params;
+      const { message } = req.body;
 
-    static async sendMessage(req: Request , res: Response){
-        try{
-            
-            const {id: receiverID } = req.params;
-            const { message } = req.body;
+      //@ts-ignore
+      const sernderId = req.user._id;
 
-            //@ts-ignore
-            const sernderId = req.user._id;
+      let conversation = await Conversation.findOne({
+        participants: {
+          $all: [sernderId, receiverID],
+        },
+      });
 
-            let conversation = await Conversation.findOne({
-                participants: {
-                    $all : [sernderId, receiverID]
-                }
-            })
+      if (!conversation) {
+        conversation = new Conversation({
+          participants: [sernderId, receiverID],
+        });
+      }
 
-            if(!conversation){
-                conversation = new Conversation({
-                    participants: [sernderId, receiverID]
-                });
-            }
+      const newMessage = new Message({
+        senderId: sernderId,
+        reciverId: receiverID,
+        message: message,
+      });
 
-            const newMessage = new Message({
-                senderId: sernderId,
-                reciverId: receiverID,
-                message: message
-            });
+      if (newMessage) {
+        conversation.messages.push(newMessage._id);
+      }
 
-            
-            if(newMessage){
-                conversation.messages.push(newMessage._id);
-            }
+      // await newMessage.save();
+      // await conversation.save(); this is run one after the other
 
-            //SOCKET FUNCTIONALITY WILL BE ADDED HERE   
+      await Promise.all([newMessage.save(), conversation.save()]); // this will run parallely and save time
 
-            // await newMessage.save();
-            // await conversation.save(); this is run one after the other
+      //SOCKET FUNCTIONALITY WILL BE ADDED HERE
+      const reciverSocketId = getReceiverSocketId(receiverID);
 
-            await Promise.all([newMessage.save(), conversation.save()]); // this will run parallely and save time
+      if (reciverSocketId) {
+        //io.to(reciverSocketId).emit("newMessage", newMessage); used to send events to a specific client
+        io.to(reciverSocketId).emit("newMessage", newMessage);
+      }
 
-            res.status(201).json({
-              message: newMessage,
-            });
+      res.status(201).json({
+        message: newMessage,
+      });
+    } catch (error) {
+      console.log(" Error in the send MEssage controller", error);
 
-        }
-        catch(error){
-
-            console.log(" Error in the send MEssage controller" , error);
-
-            res.status(500).json({
-                message:"internal server error"
-            })
-        }
+      res.status(500).json({
+        message: "internal server error",
+      });
     }
+  }
 
-    static async getMessages(req: Request , res: Response){
-        try{
+  static async getMessages(req: Request, res: Response) {
+    try {
+      const { id: userToChatId } = req.params;
+      //@ts-ignore
+      const senderId = req.user._id;
 
-            const { id: userToChatId } = req.params;
-            //@ts-ignore
-            const senderId = req.user._id;
+      const conversation = await Conversation.findOne({
+        participants: {
+          $all: [senderId, userToChatId],
+        },
+      }).populate("messages"); //Not referencing but the messages itself
 
-            const conversation = await Conversation.findOne({
-                participants: {
-                    $all: [senderId, userToChatId]
-                }
-            }).populate("messages"); //Not referencing but the messages itself
+      if (!conversation) {
+        return res.status(404).json([]);
+      }
 
-            if(!conversation){
-                return res.status(404).json([])
-            }
+      const messagges = conversation.messages;
 
-            const messagges = conversation.messages;
-
-            res.status(200).json(messagges);
-
-        }catch(error){
-            console.log("Error in the getMessages controller" , error);
-            res.status(500).json({
-                message:"internal server error"
-            })
-        }
+      res.status(200).json(messagges);
+    } catch (error) {
+      console.log("Error in the getMessages controller", error);
+      res.status(500).json({
+        message: "internal server error",
+      });
     }
+  }
 }
--- backend/socket/socket.ts
+++ backend/socket/socket.ts
@@ -5,19 +5,39 @@ const Express = require("express");
 const app = Express();
 
 const server = http.createServer(app);
-const io = new Server(server, {
+export const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
+export const getReceiverSocketId = (receiverId: string): string => {
+  return userSocketMap[receiverId];
+};
+
+const userSocketMap = {}; // {userId: socketId}
+
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+
+  if (typeof userId === "string" && userId !== "") {
+    userSocketMap[userId] = socket.id;
+  }
+
+  //socket.emit is used to send events to the client
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   //socket.on is used to listen for events both on client and server side
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
+
+    if (typeof userId === "string" && userId !== "")
+      delete userSocketMap[userId];
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
